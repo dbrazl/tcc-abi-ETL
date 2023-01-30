ae13b68d04cf6554e4fd05d91dcfee74b8d913ef
Daniel Braz <danielbrazlopes16@gmail.com>
2023-01-29 23:48:57 -0300

    refactor: create a file to export all infected models

--- ETL/src/database/index.js
+++ ETL/src/database/index.js
@@ -2,48 +2,9 @@ import Sequelize from 'sequelize';
 
 import config from '../config/database';
 
-import DimensaoClassificacaoCasoInfectado from '../models/Infected/DimensaoClassificacaoCasoInfectado';
-import DimensaoCodigoInfectado from '../models/Infected/DimensaoCodigoInfectado';
-import DimensaoComorbidadeInfectado from '../models/Infected/DimensaoComorbidadeInfectado';
-import DimensaoDataInfectado from '../models/Infected/DimensaoDataInfectado';
-import DimensaoEtniaInfectado from '../models/Infected/DimensaoEtniaInfectado';
-import DimensaoEvolucaoInfectado from '../models/Infected/DimensaoEvolucaoInfectado';
-import DimensaoFaixaEtariaInfectado from '../models/Infected/DimensaoFaixaEtariaInfectado';
-import DimensaoIdadeInfectado from '../models/Infected/DimensaoIdadeInfectado';
-import DimensaoInternacaoInfectado from '../models/Infected/DimensaoInternacaoInfectado';
-import DimensaoMacroInfectado from '../models/Infected/DimensaoMacroInfectado';
-import DimensaoMicroInfectado from '../models/Infected/DimensaoMicroInfectado';
-import DimensaoMunicipioResidenciaInfectado from '../models/Infected/DimensaoMunicipioResidenciaInfectado';
-import DimensaoOrigemDaInformacaoInfectado from '../models/Infected/DimensaoOrigemDaInformacaoInfectado';
-import DimensaoRacaInfectado from '../models/Infected/DimensaoRacaInfectado';
-import DimensaoSexoInfectado from '../models/Infected/DimensaoSexoInfectado';
-import DimensaoURSInfectado from '../models/Infected/DimensaoURSInfectado';
-import DimensaoUTIInfectado from '../models/Infected/DimensaoUTIInfectado';
-import FatoInfectado from '../models/Infected/FatoInfectado';
+import InfectedModels from '../models/Infected';
 
-
-const modelsInfectados = [
-  DimensaoClassificacaoCasoInfectado,
-  DimensaoCodigoInfectado,
-  DimensaoComorbidadeInfectado,
-  DimensaoDataInfectado,
-  DimensaoEtniaInfectado,
-  DimensaoEvolucaoInfectado,
-  DimensaoFaixaEtariaInfectado,
-  DimensaoIdadeInfectado,
-  DimensaoInternacaoInfectado,
-  DimensaoMacroInfectado,
-  DimensaoMicroInfectado,
-  DimensaoMunicipioResidenciaInfectado,
-  DimensaoOrigemDaInformacaoInfectado,
-  DimensaoRacaInfectado,
-  DimensaoSexoInfectado,
-  DimensaoURSInfectado,
-  DimensaoUTIInfectado,
-  FatoInfectado
-];
-
-const models = [...modelsInfectados];
+const models = [...InfectedModels];
 
 class Database {
   constructor() {
---
+++ ETL/src/models/Infected/index.js
@@ -0,0 +1,40 @@
+import DimensaoClassificacaoCasoInfectado from './DimensaoClassificacaoCasoInfectado';
+import DimensaoCodigoInfectado from './DimensaoCodigoInfectado';
+import DimensaoComorbidadeInfectado from './DimensaoComorbidadeInfectado';
+import DimensaoDataInfectado from './DimensaoDataInfectado';
+import DimensaoEtniaInfectado from './DimensaoEtniaInfectado';
+import DimensaoEvolucaoInfectado from './DimensaoEvolucaoInfectado';
+import DimensaoFaixaEtariaInfectado from './DimensaoFaixaEtariaInfectado';
+import DimensaoIdadeInfectado from './DimensaoIdadeInfectado';
+import DimensaoInternacaoInfectado from './DimensaoInternacaoInfectado';
+import DimensaoMacroInfectado from './DimensaoMacroInfectado';
+import DimensaoMicroInfectado from './DimensaoMicroInfectado';
+import DimensaoMunicipioResidenciaInfectado from './DimensaoMunicipioResidenciaInfectado';
+import DimensaoOrigemDaInformacaoInfectado from './DimensaoOrigemDaInformacaoInfectado';
+import DimensaoRacaInfectado from './DimensaoRacaInfectado';
+import DimensaoSexoInfectado from './DimensaoSexoInfectado';
+import DimensaoURSInfectado from './DimensaoURSInfectado';
+import DimensaoUTIInfectado from './DimensaoUTIInfectado';
+import FatoInfectado from './FatoInfectado';
+
+
+export default [
+  DimensaoClassificacaoCasoInfectado,
+  DimensaoCodigoInfectado,
+  DimensaoComorbidadeInfectado,
+  DimensaoDataInfectado,
+  DimensaoEtniaInfectado,
+  DimensaoEvolucaoInfectado,
+  DimensaoFaixaEtariaInfectado,
+  DimensaoIdadeInfectado,
+  DimensaoInternacaoInfectado,
+  DimensaoMacroInfectado,
+  DimensaoMicroInfectado,
+  DimensaoMunicipioResidenciaInfectado,
+  DimensaoOrigemDaInformacaoInfectado,
+  DimensaoRacaInfectado,
+  DimensaoSexoInfectado,
+  DimensaoURSInfectado,
+  DimensaoUTIInfectado,
+  FatoInfectado
+];
