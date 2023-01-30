ac287566709db082db09773bb7a17d350d72c477
Daniel Braz <danielbrazlopes16@gmail.com>
2023-01-30 01:10:22 -0300

    feat: create residence city dimension model

---
+++ ETL/src/models/Death/DimensaoMunicipioResidenciaObito.js
@@ -0,0 +1,23 @@
+import Sequelize, { Model } from 'sequelize';
+
+class DimensaoMunicipioResidenciaObito extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        municipio_id: {
+          type: Sequelize.INTEGER,
+          autoIncrement: true,
+          primaryKey: true
+        },
+        rotulo: Sequelize.STRING
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+}
+
+export default DimensaoMunicipioResidenciaObito;
