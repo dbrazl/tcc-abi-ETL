79ecba87187d723c406c3f896dc6e24ec930ce5a
Daniel Braz <danielbrazlopes16@gmail.com>
2023-01-30 01:23:59 -0300

    feat: create urls config file

---
+++ ETL/src/config/url.js
@@ -0,0 +1,14 @@
+export const URLs = {
+    infecteds: {
+        exams_1: 'https://drive.google.com/file/d/1G9T8MvGMXezeZymhVH8MWHwi5vg2lpK4/view?usp=sharing',
+        exams_2: 'https://drive.google.com/file/d/1fMq9nPuRThH0VrHja3PmblrUIP-x5kB-/view?usp=sharing',
+        exams_3: 'https://drive.google.com/file/d/1TdFEEHU6alEzKym23IH_Tqt2BKy7Xf_d/view?usp=sharing',
+        exams_4: 'https://drive.google.com/file/d/1a-6ypAx0PfuI0N35hu9e7TlGSTGHj0NA/view?usp=sharing',
+        exmas_5: 'https://drive.google.com/file/d/1di-lBZ_9ws2yfmh8DBYLjG1W86i7f8wL/view?usp=sharing'
+    },
+    deaths: {
+        '2020': 'https://drive.google.com/file/d/1UEcq1CN29qT326_Uj2krwPen2SKJmr1U/view?usp=sharing',
+        '2021': 'https://drive.google.com/file/d/1xOR50AIjmm95FMi9oppdU5naxUw_bOxo/view?usp=sharing',
+        '2022': 'https://drive.google.com/file/d/1mWJoIIDdSs10YUO2ra9qs5JDRSVIaxDw/view?usp=sharing'
+    }
+};
