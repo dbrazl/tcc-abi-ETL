5882d764a5d5a607df963eec059dfe7c32cc6c19
Daniel Braz <danielbrazlopes16@gmail.com>
2023-01-29 23:05:16 -0300

    feat: create infected fact model

---
+++ ETL/src/models/FatoInfectado.js
@@ -0,0 +1,38 @@
+import Sequelize, { Model } from 'sequelize';
+
+class FatoInfectado extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        id: Sequelize.NUMBER,
+        urs_FK: Sequelize.NUMBER,
+        micro_FK: Sequelize.NUMBER,
+        macro_FK: Sequelize.NUMBER,
+        municipio_FK: Sequelize.NUMBER,
+        codigo_FK: Sequelize.NUMBER,
+        origem_FK: Sequelize.NUMBER,
+        sexo_FK: Sequelize.NUMBER,
+        idade_FK: Sequelize.NUMBER,
+        faixa_etaria_FK: Sequelize.NUMBER,
+        raca_FK: Sequelize.NUMBER,
+        etnia_FK: Sequelize.NUMBER,
+        data_de_notificacao_FK: Sequelize.NUMBER,
+        data_evolucao_FK: Sequelize.NUMBER,
+        data_1_sintoma_FK: Sequelize.NUMBER,
+        data_atualizacao_FK: Sequelize.NUMBER,
+        evolucao_FK: Sequelize.NUMBER,
+        comorbidade_FK: Sequelize.NUMBER,
+        uti_FK: Sequelize.NUMBER,
+        internacao_FK: Sequelize.NUMBER,
+        classificacao_FK: Sequelize.NUMBER,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+}
+
+export default FatoInfectado;
